bb95c5f40a6b2859f14b9b2f0de22e9c7a5c0f89
Bela Bohlender <bbohlender@coconut-capital.com>
2023-10-24 18:55:18 +0200

    3.0.4

--- src/load-binary.ts
+++ src/load-binary.ts
@@ -1,12 +1,13 @@
 import { initStreaming } from "yoga-wasm-web";
-import { loadYogaBase64 } from './load-base64.js'
+import { loadYogaBase64 } from "./load-base64.js";
+
 export async function loadYoga() {
-    try{
-        const response = await fetch(`https://coconut-xr.github.io/flex/yoga.wasm`);
-        return initStreaming(response);
-    } catch (error) {
-        console.log("Cannot retrieve yoga.wasm from remote host. Error: " + error);
-        console.log("Returning local yoga.wasm file")
-        return loadYogaBase64();
-    }
+  try {
+    const response = await fetch(`https://coconut-xr.github.io/flex/yoga.wasm`);
+    return initStreaming(response);
+  } catch (error) {
+    console.log("Cannot retrieve yoga.wasm from remote host. Error: " + error);
+    console.log("Returning local yoga.wasm file");
+    return loadYogaBase64();
+  }
 }
